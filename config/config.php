faa612706557a4c71bcdc0de6379b4ff36889bb2
Bombillo2015 <juan.reyes@davivienda.com>
2024-03-15 19:35:52 -0500

    modificadores globales

--- config/config.php
+++ config/config.php
@@ -1,15 +1,23 @@
 <?php
 
 // Configuración de la base de datos
-define('DB_HOST', 'localhost');
-define('DB_USER', 'usuario_db');
-define('DB_PASS', 'contraseña_db');
-define('DB_NAME', 'nombre_db');
+// define('DB_HOST', 'localhost');
+// define('DB_USER', 'usuario_db');
+// define('DB_PASS', 'contraseña_db');
+// define('DB_NAME', 'nombre_db');
 
-// Rutas de archivos
-define('INCLUDES_PATH', __DIR__ . '/includes/');
-define('CSS_PATH', '/css/');
-define('JS_PATH', '/js/');
-define('IMAGES_PATH', '/images/');
+// // Rutas de archivos
+// define('INCLUDES_PATH', __DIR__ . '/includes/');
+// define('CSS_PATH', '/css/');
+// define('JS_PATH', '/js/');
+// define('IMAGES_PATH', '/images/');
+
+// Activadores
+
+$includeHeader = true;
+$includeFooter = true;
+
+
+$globalHead = 'includes/head.php';
 
 ?>
---
+++ includes/s-servicios.php
@@ -0,0 +1,28 @@
+<section class="a_agencia">
+            <div class="container">
+                <h2>Agencia de Diseño Web</h2>
+                <div class="desc">Somos una Agencia de Diseño Web especializada en psicología del consumidor y estrategia digital. Tenemos todo lo que necesitas para que tu marca tenga una posición sólida en Internet. <b> Conoce nuestros servicios.</b></div>
+                <div class="servicios">
+                    <a href="#" class="serv c1">
+                        <h3>Diseño Web</h3>
+                        <img width="250" height="250" src="https://codigonativo.com/wp-content/uploads/2023/06/Diseno-web-1.webp" alt="Diseño web">
+                    </a>
+                    <a href="#" class="serv c2">
+                        <h3>Auditoría Web</h3>
+                        <img width="250" height="250" src="https://codigonativo.com/wp-content/uploads/2023/06/Auditoria_card-1.webp" alt="Auditorías web">
+                    </a>
+                    <a href="#" class="serv c3">
+                        <h3>Mantenimiento Web</h3>
+                        <img width="250" height="250" src="https://codigonativo.com/wp-content/uploads/2023/06/Mantenimiento.webp" alt="Mantenimiento web">
+                    </a>
+                    <a href="#" class="serv c4">
+                        <h3>Optimización Web</h3>
+                        <img width="250" height="250" src="https://codigonativo.com/wp-content/uploads/2023/06/Optimizacion-card.webp" alt="Optimización web">
+                    </a>
+                    <a href="#" class="serv c5">
+                        <h3>Rediseño Web</h3>
+                        <img width="250" height="250" src="https://codigonativo.com/wp-content/uploads/2023/06/Rediseno-web.webp" alt="Rediseño Web">
+                    </a>
+                </div>
+            </div>
+        </section>
