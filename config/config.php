330726928c02c4a968514ec15684ec9d3883fba5
Bombillo2015 <juan.reyes@davivienda.com>
2024-03-15 19:39:50 -0500

    mods

--- config/config.php
+++ config/config.php
@@ -1,21 +1,16 @@
 <?php
 
-// Configuración de la base de datos
-// define('DB_HOST', 'localhost');
-// define('DB_USER', 'usuario_db');
-// define('DB_PASS', 'contraseña_db');
-// define('DB_NAME', 'nombre_db');
+//Configuración de la base de datos
+define('DB_HOST', 'localhost');
+define('DB_USER', 'usuario_db');
+define('DB_PASS', 'contraseña_db');
+define('DB_NAME', 'nombre_db');
 
-// // Rutas de archivos
-// define('INCLUDES_PATH', __DIR__ . '/includes/');
-// define('CSS_PATH', '/css/');
-// define('JS_PATH', '/js/');
-// define('IMAGES_PATH', '/images/');
-
-// Activadores
-
-$includeHeader = true;
-$includeFooter = true;
+// Rutas de archivos
+define('INCLUDES_PATH', __DIR__ . '/includes/');
+define('CSS_PATH', '/css/');
+define('JS_PATH', '/js/');
+define('IMAGES_PATH', '/images/');
 
 
 $globalHead = 'includes/head.php';
